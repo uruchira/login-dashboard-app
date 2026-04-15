05ed014b2ff7436c28b2295725d570c72d9dbdc7
Ruchira Udapola <uruchira@gmail.com>
2026-04-15 11:44:14 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Get started</h1>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  )
+}
+
+export default App
