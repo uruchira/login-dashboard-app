6c7c4431468cb38f26befa6108eabdabd6e7d081
Ruchira Udapola <uruchira@gmail.com>
2026-04-17 17:46:05 +0530

    tailwindcss plugin installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
-    setupFiles: './src/tests/setup.ts',
-    environment: 'jsdom',
+    setupFiles: "./src/tests/setup.ts",
+    environment: "jsdom",
   },
-})
+});
