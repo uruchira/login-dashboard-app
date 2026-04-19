e7426c5e8262f737740abe6b9094660115ae1b23
Ruchira Udapola <uruchira@gmail.com>
2026-04-19 21:21:20 +0530

    Private route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,41 @@ import Login from "./pages/auth/Login";
 import Dashboard from "./pages/dashboard";
 import ProductManagement from "./pages/dashboard/ProductManagement";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+import { useAuth } from "./contexts/AuthContext";
+
 function App() {
+  const { user } = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Login />} />
         <Route path="login" element={<Login />} />
-        <Route path="dashboard" element={<Dashboard />} />
-        <Route path="dashboard/new" element={<ProductManagement />} />
-        <Route path="dashboard/:id" element={<ProductManagement />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute user={user}>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="dashboard/new"
+          element={
+            <ProtectedRoute user={user}>
+              <ProductManagement />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="dashboard/:id"
+          element={
+            <ProtectedRoute user={user}>
+              <ProductManagement />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<h3>The page not found</h3>} />
       </Routes>
     </BrowserRouter>
